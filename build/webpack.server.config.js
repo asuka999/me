92fa2e396b1c1ea0de989f15e0a93986130a720c
asuka999 <asuka999@qq.com>
2016-10-28 04:06:05 +0800

    test deploy

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const assets = ['normalize.css', 'github-markdown-css']
 
 const externals = Object.keys(require('../package.json').dependencies)
-  .filter(item => assets.indexOf(item) < 0)
+  .filter(item => !assets.includes(item))
 
 module.exports = Object.assign({}, config, {
   entry: {
