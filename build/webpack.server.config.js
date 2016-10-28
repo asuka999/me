ab6b9788a4533f7a3a07ae2fde49e5933ff0eca1
asuka999 <asuka999@qq.com>
2016-10-29 00:41:15 +0800

    test deploy

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -5,6 +5,10 @@ const assets = ['normalize.css', 'github-markdown-css']
 const externals = Object.keys(require('../package.json').dependencies)
   .filter(item => assets.indexOf(item) < 0)
 
+const {
+  NODE_ENV = 'development',
+  ORIGIN = 'http://127.0.0.1:3000'
+} = process.env
 module.exports = Object.assign({}, config, {
   entry: {
     server: './web-app/server-entry.js'
@@ -28,9 +32,9 @@ module.exports = Object.assign({}, config, {
   plugins: [
     ...config.plugins,
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.NODE_ENV': `"${NODE_ENV}"`,
       'process.env.VUE_ENV': '"server"',
-      'process.env.ORIGIN': `"http://127.0.0.1:${process.env.PORT || 3000}"`
+      'process.env.ORIGIN': `"${ORIGIN}"`
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
